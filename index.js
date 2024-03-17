7c1eff9e9f089256265229bfd946c75abed8e4fb
ConnieAllure <conniegildemeister@gmail.com>
2024-03-17 13:40:05 -0700

    additional emojis

--- index.js
+++ index.js
@@ -457,7 +457,7 @@ app.get('/rooms/:roomId', async (req, res) => {
         console.log("readMessages: ", readMessages);
         console.log("unreadMessages: ", unreadMessages);
 
-        const emojis = ['❤️'];
+        const emojis = ['thumbs up', '100 percent', 'happy face', '❤️'];
 
         let messagesHtml1 = readMessages.map(message => {
             // Create a dropdown or buttons for emoji reactions
